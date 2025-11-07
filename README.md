d7c204c4edf4b34204fca293a227ad67cdd4a397
dzinobile <dzinobile@gmail.com>
2025-11-07 16:55:38 -0500

    Added doxygen comments to all .cpp files Updated formatting per google c++ style guide Updated README for run instructions

--- README.md
+++ README.md
@@ -3,15 +3,15 @@
 This assignment builds on the previous assignment by modifying the package for added functionality.
 
 Talker now contains a service called "FindDifference" that finds the difference between two input intergers. It also logs messages at 5 different logging levels. Whenever it publishes a message, it logs its activit at the INFO log level. It also logs a message at another log level that depends on the size of the current fibonacci value: 
-- A DEBUG message is logged when the value is under 1,000,000
-- A WARN message is logged when the value is between 1,000,000 and 1,000,000,000,000,000
-- An ERROR message warning that the number is approaching the overflow limit is logged when the value is over 1,000,000,000,000,000
-- A FATAL message is logged when the overflow limit is reached and the counter sets back to 0
+- A DEBUG_STREAM message is logged when the value is under 1,000,000
+- A WARN_STREAM message is logged when the value is between 1,000,000 and 1,000,000,000,000,000
+- An ERROR_STREAM message warning that the number is approaching the overflow limit is logged when the value is over 1,000,000,000,000,000
+- A FATAL_STREAM message is logged when the overflow limit is reached and the counter sets back to 0
 These messages are all logged using the _STREAM API.
 
-Listener was also updated to log its messages to the _STREAM API.
+Listener now outputs text in a color determined by the user. The user can put an input argument when running the launch file to change the color to red, green, yellow, blue, or white, with a default value of white.
 
-Both nodes can now be launched simultaneously using the newly created launch file.
+Both nodes can now be launched simultaneously using the newly created launch file, which takes an input argument for the text color. The service added to the publisher can be accessed using the command line.
 
 ## Assumptions
 - Using ROS2 Humble
@@ -25,7 +25,11 @@ Both nodes can now be launched simultaneously using the newly created launch fil
 ### From ROS2
 - rclcpp
 - stdmsgs
+### Python
+- launch
+- launch_ros
 ## Build / Run steps
+### Assignment 2 Deliverables
 1. Clone git repository
 ```bash
 git clone https://github.com/dzinobile/my_beginner_tutorials.git && cd my_beginner_tutorials
@@ -41,17 +45,34 @@ source /usr/share/colcon_cd/function/colcon_cd.sh
 colcon build
 source install/setup.bash
 ```
-4. Launch talker and listener nodes
+4. Launch talker and listener nodes(replace "red" with desired color: red, green, yellow, blue, or white)
 ```bash
-ros2 launch beginner_tutorials talker_listener.launch.py 
+ros2 launch beginner_tutorials talker_listener.launch.py listener_text_color:=red
 ```
-5. Call service to find difference between numbers
+5. From a new terminal, call service to find difference between numbers
 ```bash
+source install/setup.bash
 ros2 service call /subtract_two_ints beginner_tutorials/srv/FindDifference "{a: 3, b: 10}"
 ```
-
-
-
+### Run Talker and Listener Separately
+1. Complete steps 1-3 above if not already done:
+```bash
+git clone https://github.com/dzinobile/my_beginner_tutorials.git && cd my_beginner_tutorials
+source /opt/ros/humble/setup.bash
+source /usr/share/colcon_argcomplete/hook/colcon-argcomplete.bash
+source /usr/share/colcon_cd/function/colcon_cd.sh
+colcon build
+source install/setup.bash
+```
+2. Run listener (replace "red" with desired color: red, green, yellow, blue, or white)
+```bash
+ros2 run beginner_tutorials listener --ros-args -p text_color:=red
+```
+3. From a new terminal, run talker at DEBUG log level
+```bash
+source install/setup.bash
+ros2 run beginner_tutorials talker --ros-args --log-level DEBUG
+```
 
 
 
--- src/beginner_tutorials/src/subscriber_member_function.cpp
+++ src/beginner_tutorials/src/subscriber_member_function.cpp
@@ -1,62 +1,89 @@
-// // Copyright 2025 Zinobile-Corp LLC
-// //
-// // Licensed under the Apache License, Version 2.0 (the "License");
-// // you may not use this file except in compliance with the License.
-// // You may obtain a copy of the License at
-// //
-// //     http://www.apache.org/licenses/LICENSE-2.0
-// //
-// // Unless required by applicable law or agreed to in writing, software
-// // distributed under the License is distributed on an "AS IS" BASIS,
-// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// // See the License for the specific language governing permissions and
-// // limitations under the License.
-
-#include <functional>
-#include <memory>
-
+// Copyright 2025 Zinobile-Corp LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+/**
+ * @file subscriber_member_function.cpp
+ * @brief Defines MinimalSubscriber class for minimal_subscriber node
+ * @author Daniel Zinobile
+ * @date 07-Nov-2025
+ */
 #include "beginner_tutorials/srv/find_difference.hpp"
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include <functional>
+#include <memory>
 
 using std::placeholders::_1;
 
+/**
+ * @class MinimalSubscriber
+ * @brief Subscriber node that displays messages published to "topic" topic
+ */
 class MinimalSubscriber : public rclcpp::Node {
- public:
+public:
+  /**
+   * @brief Constructor for MinimalSubscriber class
+   */
   MinimalSubscriber() : Node("minimal_subscriber") {
+
+    // Create subscription to "topic" topic
     subscription_ = this->create_subscription<std_msgs::msg::String>(
         "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
-    
-    this->declare_parameter("text_color","white");
 
-
+    // Declare text_color parameter with default value of white
+    this->declare_parameter("text_color", "white");
   }
 
- private:
-  void topic_callback(const std_msgs::msg::String& msg) {
+private:
+  /**
+   * @brief Callback function logs messages to INFO_STREAM based on messages
+   * read from "topic" topic
+   * @param msg Message read from "topic" topic
+   */
+  void topic_callback(const std_msgs::msg::String &msg) {
+    // Update string color based on text_color parameter
     std::string color = this->get_parameter("text_color").as_string();
-    if (color == "red") msg_color = "\033[31m";
-    else if (color == "green") msg_color = "\033[32m";
-    else if (color == "yellow") msg_color = "\033[33m";
-    else if (color == "blue") msg_color = "\033[34m";
-    else if (color == "white") msg_color = "\033[37m";
-    else msg_color = "\033[0m"; 
+    if (color == "red")
+      msg_color = "\033[31m";
+    else if (color == "green")
+      msg_color = "\033[32m";
+    else if (color == "yellow")
+      msg_color = "\033[33m";
+    else if (color == "blue")
+      msg_color = "\033[34m";
+    else if (color == "white")
+      msg_color = "\033[37m";
+    else
+      msg_color = "\033[0m";
+
+    // Reset value is default terminal color
     const std::string reset = "\033[0m";
-    RCLCPP_INFO_STREAM(this->get_logger(), msg_color <<"I heard: " << msg.data.c_str()<<reset);
+
+    // Log message to INFO_STREAM in desired color and reset to default color
+    RCLCPP_INFO_STREAM(this->get_logger(),
+                       msg_color << "I heard: " << msg.data.c_str() << reset);
   }
   rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
   std::string msg_color;
 };
 
-int main(int argc, char* argv[]) {
+/**
+ * @brief Main function to create and spin node
+ */
+int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
-  std::string color = "white";
-  if (argc > 1) {
-    color = argv[1];
-  }
   auto node = std::make_shared<MinimalSubscriber>();
   rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
 }
-
